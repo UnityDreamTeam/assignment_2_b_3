77d73bc03633633d9b2b33a249c5f9bf3b5ff2d3
Revertigo <dekel241@gmail.com>
2020-10-31 14:17:42 +0200

    Add two players to the game and move scripts for them

---
+++ Assets/Scripts/Mover_Player1.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class Mover_Player1 : MonoBehaviour
+{
+    [Tooltip("Step size")]
+    [SerializeField]
+    float step = 0.10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    //Movement function using the arrow keys
+    private void MovePlayer()
+    {
+        Vector3 move = new Vector3(0, 0, 0);
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            move.y = step;
+        }
+        else if ((Input.GetKey(KeyCode.DownArrow)))
+        {
+            transform.position -= new Vector3(0, step, 0);
+            move.y = -step;
+        }
+        else if ((Input.GetKey(KeyCode.RightArrow)))
+        {
+            move.x = step;
+        }
+        else if ((Input.GetKey(KeyCode.LeftArrow)))
+        {
+            move.x = -step;
+        }
+
+        transform.position += move;
+    }
+}
---
+++ Assets/Scripts/Mover_Player2.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class Mover_Player2 : MonoBehaviour
+{
+    [Tooltip("Step size")]
+    [SerializeField]
+    float step = 0.10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    //Movement function using the arrow keys
+    private void MovePlayer()
+    {
+        Vector3 move = new Vector3(0, 0, 0);
+        if (Input.GetKey(KeyCode.W))
+        {
+            move.y = step;
+        }
+        else if ((Input.GetKey(KeyCode.S)))
+        {
+            transform.position -= new Vector3(0, step, 0);
+            move.y = -step;
+        }
+        else if ((Input.GetKey(KeyCode.D)))
+        {
+            move.x = step;
+        }
+        else if ((Input.GetKey(KeyCode.A)))
+        {
+            move.x = -step;
+        }
+
+        transform.position += move;
+    }
+}
